a8948e17c29e6e85c1968bcf9bf6f40445038087
Miguel Ángel Cristancho Hernández <99111487+Miguel-Cristancho@users.noreply.github.com>
2023-04-16 12:52:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function reservada1() {
+  document.getElementById("mesa1").style.display="none";
+  document.getElementById("Rmesa1").style.display="initial";
+  document.getElementById("Rmesa1").style.setProperty('box-shadow', 'initial')
+}
